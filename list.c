d228fae3944d72edca638e2b60e9f5d1c48d11bc
Michael Elkins <me@mutt.org>
2002-10-30 02:31:20 +0000

    Updated the copyright notice to allow an exception for linking with OpenSSL, which has a non-GPL compatible license.

--- list.c
+++ list.c
@@ -16,6 +16,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * As a special exception, isync may be linked with the OpenSSL library,
+ * despite that library's more restrictive license.
  */
 
 #include <ctype.h>
