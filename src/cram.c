6f2550117af9f5887efdc86268f9de5d9927ff23
Oswald Buddenhagen <ossi@users.sf.net>
2002-12-28 15:31:20 +0000

    - update copyrights. 2003 didn't begin yet, but who cares? :)

--- src/cram.c
+++ src/cram.c
@@ -1,7 +1,7 @@
 /* $Id$
  *
  * isync - IMAP4 to maildir mailbox synchronizer
- * Copyright (C) 2000-2 Michael R. Elkins <me@mutt.org>
+ * Copyright (C) 2000-2002 Michael R. Elkins <me@mutt.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- src/maildir.c
+++ src/maildir.c
@@ -1,7 +1,8 @@
 /* $Id$
  *
  * isync - IMAP4 to maildir mailbox synchronizer
- * Copyright (C) 2000-2 Michael R. Elkins <me@mutt.org>
+ * Copyright (C) 2000-2002 Michael R. Elkins <me@mutt.org>
+ * Copyright (C) 2002-2003 Oswald Buddenhagen <ossi@users.sf.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
